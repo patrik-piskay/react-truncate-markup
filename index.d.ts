e972a5487e84a692443530eec8bff048694c9edd
Henry Ing-Simmons <hing-simmons@neworbit.co.uk>
2018-05-17 08:59:14 +0100

    Add string to lineHeight type

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 export interface TruncateProps {
     lines?: number;
     ellipsis?: React.ReactNode | ((element: React.ReactNode) => React.ReactNode);
-    lineHeight?: number;
+    lineHeight?: number | string;
 }
 
 declare class TruncateMarkup extends React.Component<TruncateProps> {}
