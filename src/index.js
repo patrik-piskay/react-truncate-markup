9c1c6a6d835322ea693b314663fd34d89dc01a84
Patrik Piskay <piskay.patrik@gmail.com>
2022-07-11 15:38:29 +0100

    repository transfer changes

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ const validateTree = (node) => {
     if (process.env.NODE_ENV !== 'production') {
       /* eslint-disable no-console */
       console.error(
-        `ReactTruncateMarkup tried to render <${node.type.name} />, but truncating React components is not supported, the full content is rendered instead. Only DOM elements are supported. Alternatively, you can take advantage of the <TruncateMarkup.Atom /> component (see more in the docs https://github.com/parsable/react-truncate-markup/blob/master/README.md#truncatemarkupatom-).`,
+        `ReactTruncateMarkup tried to render <${node.type.name} />, but truncating React components is not supported, the full content is rendered instead. Only DOM elements are supported. Alternatively, you can take advantage of the <TruncateMarkup.Atom /> component (see more in the docs https://github.com/patrik-piskay/react-truncate-markup/blob/master/README.md#truncatemarkupatom-).`,
       );
       /* eslint-enable */
     }
